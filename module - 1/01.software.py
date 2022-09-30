29a17bff60443782ec2310c84b5ababf2e7e9d34
JadavDurgesh <109644031+JadavDurgesh@users.noreply.github.com>
2022-09-30 14:22:15 +0530

    nik

---
+++ module - 1/01.software.py
@@ -0,0 +1,10 @@
+'''
+1) what is software ?
+
+==> the programs and other operating information used by a computer.
+
+==> software  is a set instuctions, data or programs used to operate computers and execute specific tasks.
+
+
+
+'''
---
+++ module - 1/02.types of applications.py
@@ -0,0 +1,8 @@
+'''
+(2)  what are the types of applications ?
+
+==> * mobile application
+==> * web application
+==> * coputer application
+
+'''
